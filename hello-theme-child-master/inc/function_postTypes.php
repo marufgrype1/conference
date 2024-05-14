d0146b269b43e62af077bf293a12404c695dce67
marufgrype1 <maruf@grype.ca>
2024-05-14 16:00:10 +0600

    test

--- hello-theme-child-master/inc/function_postTypes.php
+++ hello-theme-child-master/inc/function_postTypes.php
@@ -5,17 +5,17 @@ function conference_register_post_types() {
     // CPT Options
         array(
             'labels' => array(
-                'name' => __('ML Conference'),
-                'singular_name' => __('ML Conference'),
-                'add_new' => __('Add new ML Conference'),
-                'add_new_item' => __('Add New ML Conference'),
-                'edit_item' => __('Edit ML Conference'),
-                'new_item' => __('New ML Conference'),
-                'view_item' => __('View ML Conference'),
-                'search_items' => __('Search ML Conference'),
-                'not_found' => __('No ML Conference Found'),
+                'name' => __('Conference'),
+                'singular_name' => __('Conference'),
+                'add_new' => __('Add new Conference'),
+                'add_new_item' => __('Add New Conference'),
+                'edit_item' => __('Edit Conference'),
+                'new_item' => __('New Conference'),
+                'view_item' => __('View Conference'),
+                'search_items' => __('Search Conference'),
+                'not_found' => __('No Conference Found'),
                 'not_found_in_trash' => __('No entries found in Trash'),
-                'menu_name' => __('ML Conference'),
+                'menu_name' => __('Conference'),
                 'parent_item_colon' => '',
 
             ),
@@ -23,6 +23,32 @@ function conference_register_post_types() {
             'supports' => array( 'title','editor','thumbnail' ),
             'has_archive' => true,
             'rewrite' => array('slug' => 'conference-archive'),
+            'menu_icon' => 'dashicons-awards',
+        )
+    );
+    register_post_type( 'ml_speaker',
+    // CPT Options
+        array(
+            'labels' => array(
+                'name' => __('Speaker'),
+                'singular_name' => __('Speaker'),
+                'add_new' => __('Add new Speaker'),
+                'add_new_item' => __('Add New Speaker'),
+                'edit_item' => __('Edit Speaker'),
+                'new_item' => __('New Speaker'),
+                'view_item' => __('View Speaker'),
+                'search_items' => __('Search Speaker'),
+                'not_found' => __('No Speaker Found'),
+                'not_found_in_trash' => __('No entries found in Trash'),
+                'menu_name' => __('Speaker'),
+                'parent_item_colon' => '',
+
+            ),
+            'public' => false,
+            'publicly_queryable'    => false,
+            'supports' => array( 'title','editor','thumbnail' ),
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'speaker-archive'),
             'menu_icon' => 'dashicons-awards',
         )
     );
